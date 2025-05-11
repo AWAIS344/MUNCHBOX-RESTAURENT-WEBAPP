06e28bdf679be95003bc1e2a4068a07beec32294
awais khan <awaisalishah02@gmail.com>
2025-05-11 22:58:13 +0500

    DashBoard Settings

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from .form import RegistartionForm
 
 # Create your views here.
 def register(request):
-    return render(request,"accounts/login.html")
+    form=RegistartionForm()
+    context={"form":form}
+    return render(request,"accounts/login.html",context)
