522e111d24b946967d8cf4f959e6006b2053cf79
awais khan <awaisalishah02@gmail.com>
2025-05-13 20:38:46 +0500

    login and registration setup

--- accounts/views.py
+++ accounts/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from django.urls import reverse
+from django.http import HttpResponseRedirect
+from django.contrib.auth import login,authenticate,logout
 from .form import RegistartionForm,LoginFrom
 
 # Create your views here.
@@ -9,5 +12,25 @@ def register(request):
 
 def login(request):
     form=LoginFrom()
-    context={"form":form}
-    return render(request,"accounts/login.html",context)
+    home = reverse("home")
+
+    if request.user.is_authenticated:
+        return HttpResponseRedirect(home)
+    else:
+
+        if request.method == 'POST':
+            form=LoginFrom(request=request,data=request.POST)
+            if form.is_valid():
+                username=form.cleaned_data['username']
+                password=form.changed_data['password']
+
+                print(username)
+                print(password)
+                user=authenticate(username=username,password=password)
+                if user is not None:
+                    login(request,user)
+                    return HttpResponseRedirect(home)
+        else:
+            form=LoginFrom()
+        context={"form":form}
+        return render(request,"accounts/login.html",context)
