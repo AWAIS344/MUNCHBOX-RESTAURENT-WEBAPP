141c5cc73433fc94662d0d96faa5325a82448fc3
awais khan <awaisalishah02@gmail.com>
2025-05-13 19:57:47 +0500

    login and registration setup

--- accounts/views.py
+++ accounts/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
-from .form import RegistartionForm
+from .form import RegistartionForm,LoginFrom
 
 # Create your views here.
 def register(request):
     form=RegistartionForm()
+    context={"form":form}
+    return render(request,"accounts/registration.html",context)
+
+def login(request):
+    form=LoginFrom()
     context={"form":form}
     return render(request,"accounts/login.html",context)
