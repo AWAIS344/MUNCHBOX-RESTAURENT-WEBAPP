15e4def8ad59ae75c63b6d267a22521289bf0429
awais khan <awaisalishah02@gmail.com>
2025-05-19 22:03:49 +0500

    Url Modifications

--- Order/urls.py
+++ Order/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
-from .views import OrderCheckout
+from .views import OrderCheckout,OrderDetails
 urlpatterns = [
     
     path('checkout/', OrderCheckout , name="checkout" ),
+    path('orderdetail/', OrderDetails , name="order_detail" ),
 ]
--- Order/views.py
+++ Order/views.py
@@ -4,3 +4,7 @@ from django.shortcuts import render
 def OrderCheckout(request):
     context={}
     return render(request,"order/checkout.html",context)
+
+def OrderDetails(request):
+    context={}
+    return render(request,"order/order_detail.html",context)
