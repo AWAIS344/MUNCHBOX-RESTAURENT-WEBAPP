0885c7625bde0274ca98c2831cdbaeceffb0f262
awais khan <awaisalishah02@gmail.com>
2025-05-20 23:38:36 +0500

    GeoLocator App Added

--- Geolocator/views.py
+++ Geolocator/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 # Create your views here.
 def GeoLocator(request):
     context={}
-    return render(request,"ExDeals/ex_deals.html",context)
+    return render(request,"geolocator/geolocator.html",context)
