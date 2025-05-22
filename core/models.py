4400f1808600f67a70f7b9b561752569590b9ca6
awais khan <awaisalishah02@gmail.com>
2025-05-22 23:30:11 +0500

    Models Updations

--- core/models.py
+++ core/models.py
@@ -96,6 +96,15 @@ class PaymentMethod(models.Model):
     def __str__(self):
         return f"{self.get_method_display()} - {self.user.username}"
     
+class GiftCard(models.Model):
+    code = models.CharField(max_length=20, unique=True)
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
+    is_used = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Gift Card - {self.code}"
+    
     
 
 class Order(models.Model):
